6b064840080e4beac37e00240ab136e1ca51359d
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-16 13:03:09 +0000

    Move MemberDiv to separate file

---
+++ lib/member_div.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+class MemberDiv < Scraped::HTML
+  field :id do
+    email.sub(/@.*/, '')
+  end
+
+  field :name do
+    noko.css('#org_title').text.tidy
+  end
+
+  field :image do
+    noko.css('div#org_left img/@src').text
+  end
+
+  field :party do
+    noko.css('#orgi_right').first.text.tidy
+  end
+
+  field :email do
+    js = noko.css('#contact script').text.gsub(/document.getElementById.*?;/, '')
+    var = js[/var (addy.*?)=/, 1]
+    CGI.unescapeHTML(ExecJS.exec("#{js}; return #{var}"))
+  end
+
+  fied :source do
+    url
+  end
+end
